d81d86d7c870615fa2235bbc8cae3e860b0fe50a
m-bender1 <mattbender252@yahoo.com>
2021-12-05 18:13:27 -0500

    changed keycode to keyboardEvent.key (deprecated)

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ var taskList = document.getElementById("taskList");
 // for add button to work with enter key
 let taskInputArea = document.getElementById("inputTask");
 taskInputArea.addEventListener("keyup", function(event) {
-   if (event.keyCode === 13) {
+   if (event.keyboardEvent.key === 13) {
     event.preventDefault();
     addBtn.click();
    }
